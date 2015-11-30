d349d8766ca04e15ca98c2304ef0835dcf6cc837
Youxian <youxian.chen@fiziico.com>
2015-11-30 18:08:28 +0800

    add swipe function to list

--- app/src/main/java/com/example/youxian/filepicker/MainActivity.java
+++ app/src/main/java/com/example/youxian/filepicker/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.youxian.filepicker;
 
 import android.app.Activity;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.File;
@@ -30,6 +34,14 @@ public class MainActivity extends Activity {
     private List<File> mFiles;
     private File currentFile;
     private File rootFile;
+
+    private VelocityTracker mVelocityTracker;
+    private int mSwipingSlop;
+
+    private boolean hasSwiped = false;
+    private boolean mSwiping = false;
+    private boolean openState = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,7 +98,7 @@ public class MainActivity extends Activity {
     }
 
     private class FileListAdapter extends BaseAdapter {
-
+        private int mWidth;
         private List<File> mListFiles;
         private LayoutInflater mInflater;
         public FileListAdapter(List<File> files) {
@@ -111,33 +123,128 @@ public class MainActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-                final File file = mListFiles.get(position);
-                Log.d(TAG, "file: " + file.getName() + " is folder: " + file.isDirectory());
-                if (!file.isDirectory()) {
-                    convertView = mInflater.inflate(R.layout.listrow_file, null);
-                    TextView titleFile = (TextView) convertView.findViewById(R.id.title_file_item);
-                    final CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox_file_item);
-                    titleFile.setText(file.getName());
-                    convertView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            checkBox.performClick();
-                        }
-                    });
-                } else {
-                    convertView = mInflater.inflate(R.layout.listrow_folder, null);
-                    TextView titleFolder = (TextView) convertView.findViewById(R.id.title_folder_item);
-                    titleFolder.setText(file.getName());
-                    convertView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Log.d(TAG, "onClick");
-                            showDirectory(file);
-                        }
-                    });
-                }
-
+            final File file = mListFiles.get(position);
+            Log.d(TAG, "file: " + file.getName() + " is folder: " + file.isDirectory());
+            if (!file.isDirectory()) {
+                convertView = mInflater.inflate(R.layout.listrow_file, null);
+                TextView titleFile = (TextView) convertView.findViewById(R.id.title_file_item);
+                TextView sizeFile = (TextView) convertView.findViewById(R.id.size_file_item);
+                final CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox_file_item);
+                checkBox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "Checkbox on clicked");
+                    }
+                });
+                titleFile.setText(file.getName());
+                sizeFile.setText(file.length() / 1024 + " KB");
+                mWidth = 300;
+                convertView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        checkBox.performClick();
+                        Log.d(TAG, "onClick");
+                    }
+                });
+                RelativeLayout relativeLayout = (RelativeLayout) convertView.findViewById(R.id.layout_file_item);
+                relativeLayout.setOnTouchListener(mTouchListener);
+            } else {
+                convertView = mInflater.inflate(R.layout.listrow_folder, null);
+                TextView titleFolder = (TextView) convertView.findViewById(R.id.title_folder_item);
+                titleFolder.setText(file.getName());
+                convertView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "onClick");
+                        showDirectory(file);
+                    }
+                });
+            }
             return convertView;
         }
+
+        private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+            private float mDownX;
+            private int mSlop = -1;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (mSlop < 0) {
+                    mSlop = ViewConfiguration.get(MainActivity.this).
+                            getScaledTouchSlop();
+                    Log.d(TAG, "slop: " + mSlop);
+                }
+                switch (event.getActionMasked()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d(TAG, "Action down");
+                        mDownX = event.getRawX();
+                        mVelocityTracker = VelocityTracker.obtain();
+                        mVelocityTracker.addMovement(event);
+                        break;
+
+                    case MotionEvent.ACTION_CANCEL:
+                        Log.d(TAG, "Action cancel");
+                        v.animate().translationX(0)
+                                .alpha(1)
+                                .setDuration(450);
+                        mVelocityTracker.recycle();
+                        mVelocityTracker = null;
+                        mSwiping = false;
+                        mDownX = 0;
+                        hasSwiped = false;
+                        openState = false;
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        Log.d(TAG, "Action move");
+                        mVelocityTracker.addMovement(event);
+                        float deltaX = event.getRawX() - mDownX;
+
+                        if (!mSwiping && Math.abs(deltaX) > mSlop && deltaX < 0) {
+                            mSwiping = true;
+                            mSwipingSlop = (deltaX > 0 ? mSlop : -mSlop);
+                            mFilesList.requestDisallowInterceptTouchEvent(true);
+                        }
+                        if (mSwiping) {
+                            Log.d(TAG, "swiping");
+                            hasSwiped = true;
+                            mFilesList.invalidate();
+                            if (Math.abs(deltaX) < mWidth + mSlop
+                                    && deltaX <= mSwipingSlop) {
+                                Log.d(TAG, "view move");
+                                v.setTranslationX(deltaX - mSwipingSlop);
+                                openState = false;
+                            }
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        Log.d(TAG, "Action up");
+                        if (openState) {
+                            v.animate().translationX(0)
+                                    .alpha(1)
+                                    .setDuration(450);
+                            openState = false;
+                        } else if (!openState && hasSwiped){
+                            v.animate().translationX(-mWidth)
+                                    .alpha(1)
+                                    .setDuration(450);
+                            openState = true;
+                        } else {
+                            Log.d(TAG, "layout clicked");
+                        }
+                        if(mVelocityTracker != null) mVelocityTracker.recycle();
+                        mVelocityTracker = null;
+                        mDownX = 0;
+                        mSwiping = false;
+                        hasSwiped = false;
+                        break;
+
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        };
     }
 }
